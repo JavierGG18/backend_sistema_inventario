23aba9ce336e7131b0a41375a42e88288ab6401b
Javier Garcia <aj.garcia20@info.uas.edu.mx>
2025-02-18 22:58:04 -0700

    primer commit

--- SistemaInventario/obj/Debug/net9.0/SistemaInventario.AssemblyInfo.cs
+++ SistemaInventario/obj/Debug/net9.0/SistemaInventario.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+959d81d03e10860456393560738e0220e9cc66e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
